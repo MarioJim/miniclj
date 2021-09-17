483bee3696cef37923ab51e900bdb3f6a1bb4b54
MarioJim <mario.emilio.j@gmail.com>
2021-09-16 21:12:02 -0500

    Implement initial parser

---
+++ src/ast.rs
@@ -0,0 +1,151 @@
+use std::{
+    collections::{HashMap, HashSet},
+    fmt::{Display, Formatter},
+    hash::Hash,
+};
+
+use num::BigRational;
+
+pub type SExprs = Vec<Box<SExpr>>;
+
+#[derive(Debug)]
+pub enum SExpr {
+    Expr(SExprs),
+    Lambda(SExprs),
+    List(SExprs),
+    Vector(SExprs),
+    Set(HashSet<Box<Atom>>),
+    Map(HashMap<String, Box<SExpr>>),
+    Atom(Box<Atom>),
+}
+
+impl Display for SExpr {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SExpr::Expr(exprs) => {
+                write!(f, "(")?;
+                let mut it = exprs.iter();
+                if let Some(e) = it.next() {
+                    write!(f, "{}", e)?;
+                }
+                for expr in it {
+                    write!(f, " {}", expr)?;
+                }
+                write!(f, ")")
+            }
+            SExpr::Lambda(exprs) => {
+                write!(f, "#(")?;
+                let mut it = exprs.iter();
+                if let Some(e) = it.next() {
+                    write!(f, "{}", e)?;
+                }
+                for expr in it {
+                    write!(f, " {}", expr)?;
+                }
+                write!(f, ")")
+            }
+            SExpr::List(exprs) => {
+                write!(f, "'(")?;
+                let mut it = exprs.iter();
+                if let Some(e) = it.next() {
+                    write!(f, "{}", e)?;
+                }
+                for expr in it {
+                    write!(f, " {}", expr)?;
+                }
+                write!(f, ")")
+            }
+            SExpr::Vector(exprs) => {
+                write!(f, "[")?;
+                let mut it = exprs.iter();
+                if let Some(e) = it.next() {
+                    write!(f, "{}", e)?;
+                }
+                for expr in it {
+                    write!(f, " {}", expr)?;
+                }
+                write!(f, "]")
+            }
+            SExpr::Set(exprs) => {
+                write!(f, "{{")?;
+                let mut it = exprs.iter();
+                if let Some(e) = it.next() {
+                    write!(f, "{}", e)?;
+                }
+                for expr in it {
+                    write!(f, " {}", expr)?;
+                }
+                write!(f, "}}")
+            }
+            SExpr::Map(_) => todo!(),
+            SExpr::Atom(a) => write!(f, "{}", a),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub enum Atom {
+    Identifier(String),
+    String(String),
+    Number(BigRational),
+    Factor(FactorOp),
+    Comparison(ComparisonOp),
+    LambdaArg,
+    Nil,
+}
+
+impl Display for Atom {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Atom::Identifier(s) => write!(f, "{}", s),
+            Atom::String(s) => write!(f, "{}", s),
+            Atom::Number(n) => write!(f, "{}", n),
+            Atom::Factor(o) => write!(f, "{}", o),
+            Atom::Comparison(o) => write!(f, "{}", o),
+            Atom::LambdaArg => write!(f, "%"),
+            Atom::Nil => write!(f, "nil"),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub enum FactorOp {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+
+impl Display for FactorOp {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            FactorOp::Add => write!(f, "+"),
+            FactorOp::Sub => write!(f, "-"),
+            FactorOp::Mul => write!(f, "*"),
+            FactorOp::Div => write!(f, "/"),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+pub enum ComparisonOp {
+    Eq,
+    Ne,
+    Gt,
+    Lt,
+    Ge,
+    Le,
+}
+
+impl Display for ComparisonOp {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ComparisonOp::Eq => write!(f, "="),
+            ComparisonOp::Ne => write!(f, "!="),
+            ComparisonOp::Gt => write!(f, ">"),
+            ComparisonOp::Lt => write!(f, "<"),
+            ComparisonOp::Ge => write!(f, ">="),
+            ComparisonOp::Le => write!(f, "<="),
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::env::args;
+use std::fs::read_to_string;
+
+use lalrpop_util::lalrpop_mod;
+use regex::Regex;
+
+lalrpop_mod!(pub miniclj);
+mod ast;
+
+fn main() -> Result<(), &'static str> {
+    let r = Regex::new("^$").unwrap();
+    for v in &["a", "?", "a+sd'a*s>d<!?", "as\n", "0asdf", "sdf,asdf"] {
+        println!("{} {}", v, r.is_match(v));
+    }
+    let mut args = args();
+    let third_arg = args
+        .nth(1)
+        .ok_or("Expected path to input file as first argument")?;
+
+    let input = read_to_string(third_arg).unwrap();
+    match miniclj::SExprsParser::new().parse(&input) {
+        Ok(exprs) => {
+            for expr in exprs {
+                println!("{}", expr)
+            }
+        }
+        Err(e) => println!("{:#?}", e),
+    }
+    Ok(())
+}
