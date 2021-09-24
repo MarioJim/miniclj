fc1b8bfc182e9b21083359cf24c75abddb5a3e8f
MarioJim <mario.emilio.j@gmail.com>
2021-09-23 20:13:05 -0500

    Implement typecasting functions

--- src/callables/typecastingfns.rs
+++ src/callables/typecastingfns.rs
@@ -1,7 +1,12 @@
+use num::{Rational64, Signed};
+
 use crate::{
     callables::{Callable, ExecutionResult},
+    miniclj::NumberLiteralParser,
     Scope, Value,
 };
+
+use super::RuntimeError;
 
 #[derive(Debug, Clone)]
 struct NumberCast;
@@ -11,8 +16,25 @@ impl Callable for NumberCast {
         "num"
     }
 
-    fn call(&self, _: &[Value], _: &Scope) -> ExecutionResult {
-        todo!()
+    fn call(&self, args: &[Value], _: &Scope) -> ExecutionResult {
+        if args.len() != 1 {
+            return Err(RuntimeError::ArityError(self.name(), "<string>"));
+        }
+        if let Value::String(s) = &args[0] {
+            match NumberLiteralParser::new().parse(s) {
+                Ok(n) => Ok(Value::Number(n)),
+                Err(_) => Err(RuntimeError::GenericError(format!(
+                    "The string \"{}\" couldn't be parsed as a number",
+                    s
+                ))),
+            }
+        } else {
+            Err(RuntimeError::WrongArgument(
+                self.name(),
+                "a string",
+                args[0].type_str(),
+            ))
+        }
     }
 }
 
@@ -25,9 +47,86 @@ impl Callable for StringCast {
     fn name(&self) -> &'static str {
         "str"
     }
-    fn call(&self, _: &[Value], _: &Scope) -> ExecutionResult {
-        todo!()
+
+    fn call(&self, args: &[Value], _: &Scope) -> ExecutionResult {
+        Ok(Value::String(
+            args.iter().map(|v| format!("{}", v)).collect(),
+        ))
     }
 }
 
 display_for_callable!(StringCast);
+
+#[derive(Debug, Clone)]
+struct Ord;
+
+impl Callable for Ord {
+    fn name(&self) -> &'static str {
+        "ord"
+    }
+
+    fn call(&self, args: &[Value], _: &Scope) -> ExecutionResult {
+        if args.len() != 1 {
+            return Err(RuntimeError::ArityError(self.name(), "<string>"));
+        }
+        if let Value::String(s) = &args[0] {
+            match s.chars().next() {
+                Some(c) => Ok(Value::Number(Rational64::from_integer(c as i64))),
+                None => Err(RuntimeError::WrongArgument(
+                    self.name(),
+                    "a string with at least one character",
+                    "an empty string",
+                )),
+            }
+        } else {
+            Err(RuntimeError::WrongArgument(
+                self.name(),
+                "a string",
+                args[0].type_str(),
+            ))
+        }
+    }
+}
+
+display_for_callable!(Ord);
+
+#[derive(Debug, Clone)]
+struct Chr;
+
+impl Callable for Chr {
+    fn name(&self) -> &'static str {
+        "chr"
+    }
+
+    fn call(&self, args: &[Value], _: &Scope) -> ExecutionResult {
+        if args.len() != 1 {
+            return Err(RuntimeError::ArityError(self.name(), "<number>"));
+        }
+        if let Value::Number(n) = &args[0] {
+            if !n.is_integer() || n.is_negative() {
+                Err(RuntimeError::WrongArgument(
+                    self.name(),
+                    "a positive integer",
+                    "a decimal or negative integer",
+                ))
+            } else {
+                match char::from_u32(*n.numer() as u32) {
+                    Some(c) => Ok(Value::String(String::from(c))),
+                    None => Err(RuntimeError::GenericError(format!(
+                        "{} couldn't convert the number {} to a valid character",
+                        self.name(),
+                        n.numer()
+                    ))),
+                }
+            }
+        } else {
+            Err(RuntimeError::WrongArgument(
+                self.name(),
+                "a number",
+                args[0].type_str(),
+            ))
+        }
+    }
+}
+
+display_for_callable!(Chr);
