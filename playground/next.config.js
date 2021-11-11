c99bdbc4f1ba5b7bab1c3a9663ee2bf98cd93b98
MarioJim <mario.emilio.j@gmail.com>
2021-11-11 00:14:02 -0600

    Fix base path for the playground

--- playground/next.config.js
+++ playground/next.config.js
@@ -2,6 +2,7 @@ const removeImports = require('next-remove-imports')();
 
 /** @type {import('next').NextConfig} */
 module.exports = removeImports({
+  basePath: '/miniclj',
   reactStrictMode: true,
   webpack: (config) => {
     config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
